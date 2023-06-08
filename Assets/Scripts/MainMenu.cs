b181d9da406462996b3af621c8fbb1e2ec1589ab
DESKTOP-DP87FAN\Felix <felixmuraguri@gmail.com>
2023-06-08 22:58:24 +0300

    Add project files.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1); 
+    }
+
+    public void Quit()
+    {
+       #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+       #else
+        Application.Quit();
+       #endif
+    }
+}
